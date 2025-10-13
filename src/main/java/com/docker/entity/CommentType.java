aaee8adf5ec5906cce940598a4ce841a093a4066
pliplop1 <belie80@hotmail.fr>
2025-10-13 14:44:31 +0200

    feat: Ajout de l'entité Playlist et support TRACK pour commentaires
    Création de l'entité Playlist:
    - Gestion de playlists de musiques avec ordre des tracks
    - Support de couverture d'image et description
    - Options de visibilité (public/featured)
    - Méthodes utilitaires (durée totale, comptage, réordonnancement)
    - Relations ManyToMany avec Track via table playlist_tracks
    
    Mise à jour CommentType:
    - Ajout du type TRACK pour commentaires sur musiques
    
    Création PlaylistRepository:
    - Méthodes de recherche par visibilité et créateur

--- src/main/java/com/docker/entity/CommentType.java
+++ src/main/java/com/docker/entity/CommentType.java
@@ -4,5 +4,6 @@ public enum CommentType {
     CONCERT,    // Commentaire sur un concert
     ARTICLE,    // Commentaire sur un article
     VIDEO,      // Commentaire sur une vidéo
+    TRACK,      // Commentaire sur une musique
     PHOTO       // Commentaire sur une photo
 }
